1826e4c78113d2f389b4e8a01754797efa53bfa3
Nikita Baboshin <baboshinikki90@gmail.com>
2025-12-06 00:21:46 +0400

    feat: add order dto

---
+++ src/main/java/dto/OrderCreateDto.java
@@ -0,0 +1,20 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreateDto {
+    private List<OrderItemDto> orderItems;
+    private String address;
+    private String phone;
+    private String comment;
+    private String nameRecipient;
+}
--- src/main/java/ru/nikita/QuickOrderSystem/entity/Order.java
+++ src/main/java/ru/nikita/QuickOrderSystem/entity/Order.java
@@ -29,6 +29,8 @@ public class Order {
 
     private String address;
 
+    private String comment;
+
     private String phone;
 
     private String nameRecipient;
